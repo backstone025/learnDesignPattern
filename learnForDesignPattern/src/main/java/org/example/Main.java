df56c409981e3a878e3e175545a12116399d9c8a
Bea gyeong seok <gsr025silver.naver.com>
2025-06-24 16:43:54 +0900

    decorator pattern(3) - Java IO로 stream에 잇는 대문자를 전부 소문자로 바꿔주는 decorator

--- learnForDesignPattern/src/main/java/org/example/Main.java
+++ learnForDesignPattern/src/main/java/org/example/Main.java
@@ -1,29 +1,29 @@
 package org.example;
 
-import org.example.ExampleOfDecoratorPattern.coffeeOrderSystem.*;
+import org.example.ExampleOfDecoratorPattern.JavaIODecorator.*;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Main {
-    public static void main(String[] args) {
-        Beverage beverage = new Espresso();
-        System.out.println(beverage.getDescription() + "$" + beverage.cost());
+    public static void main(String[] args){
+        int c;
 
-        Beverage beverage1 = new DarkRoast();
-        beverage1 = new Mocha(beverage1);
-        beverage1 = new Mocha(beverage1);
-        beverage1 = new Whip(beverage1);
-        System.out.println(beverage1.getDescription() + "$" + beverage1.cost());
-
-        Beverage beverage2 = new HouseBlend();
-        beverage2 = new Soy(beverage2);
-        beverage2 = new Mocha(beverage2);
-        beverage2 = new Whip(beverage2);
-        System.out.println(beverage2.getDescription() + " (size : " + beverage2.getSize() + ")" + "$" + beverage2.cost());
-
-        Beverage beverage3 = new HouseBlend();
-        beverage3.setSize(Beverage.Size.VENTI);
-        beverage3 = new Soy(beverage3);
-        beverage3 = new Mocha(beverage3);
-        beverage3 = new Whip(beverage3);
-        System.out.println(beverage3.getDescription() + " (size : " + beverage3.getSize() + ")" + "$" + beverage3.cost());
+        try {
+            InputStream in = new LowerCaseInputStream(
+                    new BufferedInputStream(
+                            // 실행 시 file 경로는 따로 찾아서 넣을 것
+                            new FileInputStream("/...<file directory>.../src/main/java/org/example/ExampleOfDecoratorPattern/JavaIODecorator/test.txt")));
+            while ((c=in.read())>=0){
+                System.out.print((char) c);
+            }
+            System.out.println();
+            in.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
