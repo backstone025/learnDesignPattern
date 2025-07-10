d9a6c58e4bab582fa43d4d5c8ad9cb70526145be
Bea gyeong seok <gsr025silver.naver.com>
2025-07-10 15:16:49 +0900

    singletone pattern(3) - 기존 Lazy instantiation기법에서 multithreading으로 인한 문제점을 해결하기 위한 3가지 방법을 구현했다. 	1. getInstance() method를 동기화 하는 방법 	2. instance를 시작하자마자 만드는 방법 	3. DCL(double-check locking)을 사용하는 방법

--- learnForDesignPattern/src/main/java/org/example/ExampleOfSingletonPattern/Singleton.java
+++ learnForDesignPattern/src/main/java/org/example/ExampleOfSingletonPattern/Singleton.java
@@ -3,11 +3,37 @@ package org.example.ExampleOfSingletonPattern;
 public class Singleton {
     private static Singleton uniqueInstance;
 
-    private Singleton() {}
-    public static Singleton getInstance(){
+    private Singleton() {
+    }
+
+    // 방법 1 : getInstance()를 동기화 하기
+    /*
+    public static synchronized Singleton getInstance(){
         if(uniqueInstance == null){
             uniqueInstance = new Singleton();
         }
         return uniqueInstance;
     }
+     */
+    // 방법 2 : 처음부터 instance를 만드는 방법
+    /*
+    private static Singleton uniqueInstance = new Singletone();
+
+    public static Singleton getInstance(){
+        return uniqueInstance;
+    }
+     */
+    // 방법 3 : DCL(double-check locking)기법 사용하기
+    /*
+    public static Singleton getInstance() {
+        if (uniqueInstance == null) {
+            synchronized (Singleton.class) {
+                if (uniqueInstance == null) {
+                    uniqueInstance = new Singleton();
+                }
+            }
+        }
+        return uniqueInstance;
+    }
+     */
 }
