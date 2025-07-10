820bdb830c9401319783ae3770b4621edc95000b
Bea gyeong seok <gsr025silver.naver.com>
2025-07-10 16:10:18 +0900

    singletone pattern(3) 수정

--- learnForDesignPattern/src/main/java/org/example/ExampleOfSingletonPattern/Singleton.java
+++ learnForDesignPattern/src/main/java/org/example/ExampleOfSingletonPattern/Singleton.java
@@ -1,13 +1,14 @@
 package org.example.ExampleOfSingletonPattern;
 
 public class Singleton {
+
+    // 방법 1 : getInstance()를 동기화 하기
+    /*
     private static Singleton uniqueInstance;
 
     private Singleton() {
     }
 
-    // 방법 1 : getInstance()를 동기화 하기
-    /*
     public static synchronized Singleton getInstance(){
         if(uniqueInstance == null){
             uniqueInstance = new Singleton();
@@ -19,12 +20,20 @@ public class Singleton {
     /*
     private static Singleton uniqueInstance = new Singletone();
 
+    private Singleton() {
+    }
+
     public static Singleton getInstance(){
         return uniqueInstance;
     }
      */
     // 방법 3 : DCL(double-check locking)기법 사용하기
     /*
+    private volatile static Singleton uniqueInstance;
+
+    private Singleton() {
+    }
+
     public static Singleton getInstance() {
         if (uniqueInstance == null) {
             synchronized (Singleton.class) {
