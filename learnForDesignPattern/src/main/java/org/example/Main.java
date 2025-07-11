e9ab42f3df754c2a7b0c486c99e549ecd3007abe
Bea gyeong seok <gsr025silver.naver.com>
2025-07-11 16:58:52 +0900

    singletone pattern(4) - 열거형(enum)을 사용해 singleton pattern을 구현하였다. - 기존 동시성 문제, reflection, 직렬화, 역직렬화문제를 해결했다.

--- learnForDesignPattern/src/main/java/org/example/Main.java
+++ learnForDesignPattern/src/main/java/org/example/Main.java
@@ -1,19 +1,9 @@
 package org.example;
 
-import org.example.ExampleOfFactoryPattern.AbstractFactoryPattern.Pizza;
-import org.example.ExampleOfFactoryPattern.AbstractFactoryPattern.PizzaStore;
-import org.example.ExampleOfFactoryPattern.AbstractFactoryPattern.NYPizzaStore;
-import org.example.ExampleOfFactoryPattern.AbstractFactoryPattern.ChicagoPizzaStore;
+import org.example.ExampleOfSingletonPattern.Singleton;
 
 public class Main {
     public static void main(String[] args) {
-        PizzaStore nyStore = new NYPizzaStore();
-        PizzaStore chicagoStore = new ChicagoPizzaStore();
-
-         Pizza pizza = nyStore.orderPizza("cheese");
-         System.out.println("에단이 주문한" + pizza.getName()+"\n");
-
-         pizza = chicagoStore.orderPizza("cheese");
-         System.out.println("조엘이 주문한" + pizza.getName()+"\n");
+        Singleton singleton = Singleton.UNIQUE_INSTANCE;
     }
 }
