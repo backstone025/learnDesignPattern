5a81c2cc6fb59ab0dcaa2b8b713bc6012836ed8f
Bea gyeong seok <gsr025silver.naver.com>
2025-09-05 09:58:30 +0900

    Revert "compound pattern(4)"
    This reverts commit 42976b248d8ed4449eb0e8fd84a36987b0b5a514.

--- learnForDesignPattern/src/main/java/org/example/ExampleOfComposite/AbstractDuckFactory.java
+++
@@ -1,9 +0,0 @@
-package org.example.ExampleOfComposite;
-
-public abstract class AbstractDuckFactory {
-    public abstract Quackable createMallarDuck();
-    public abstract Quackable createRedheadDuck();
-    public abstract Quackable createDuckCall();
-    public abstract Quackable createRubberDuck();
-    public abstract Quackable createGooseDuck();
-}
